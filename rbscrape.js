fdff646f7c75bb5ed872ede4fb726471d7d4f488
amazingjoe <amazingjoe@users.noreply.github.com>
2021-07-17 22:45:35 -0700

    Update rbscrape.js

--- rbscrape.js
+++ rbscrape.js
@@ -24,7 +24,7 @@
     }
 
     function main() {
-        createScript("https://cdn.jsdelivr.net/gh/amazingjoe/rbscrape@main/js/rbs_bootstrap.js", function() {
+        createScript("https://cdn.jsdelivr.net/gh/amazingjoe/rbscrape@0.01/js/rbs_bootstrap.js", function() {
             bootstrap(() => {
                 console.log('done....');
                 createSearchBar(() => {
