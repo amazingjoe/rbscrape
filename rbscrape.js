f41a01676e58bdeae7bef25f933e3fa56922e9f0
amazingjoe <amazingjoe@users.noreply.github.com>
2021-07-30 23:41:47 -0700

    0.069

--- rbscrape.js
+++ rbscrape.js
@@ -30,7 +30,7 @@
     }    
 
     function main() {
-        createScript("https://cdn.jsdelivr.net/gh/amazingjoe/rbscrape@0.068/js/rbs_bootstrap.js", function() {
+        createScript("https://cdn.jsdelivr.net/gh/amazingjoe/rbscrape@0.069/js/rbs_bootstrap.js", function() {
             bootstrap(() => {
                 console.log('done....');
                 removeJunk(() => {    
